37b0035b5692984c34ef5bcad8d1101af80a3212
Matthew Spence <msaspence@gmail.com>
2012-01-21 13:50:04 +0000

    Added shorthand class

---
+++ classes/p.php
@@ -0,0 +1,45 @@
+<?php
+
+class P {
+
+	static public function m($text)
+	{
+		return \Profiler::mark($text);
+	}
+
+	static public function st($id,$text=null)
+	{
+		return \Profiler::start_timer($id,$text);
+	}
+
+	static public function spt($id)
+	{
+		return \Profiler::stop_timer($id);
+	}
+
+	static public function mm($object=false, $text = 'Memory usage')
+	{
+		return \Profiler::mark_memory($object, $text);
+	}
+
+	public static function c($text)
+	{
+		return \Profiler::console($text);
+	}
+
+	public static function l($type,$text,$time=null)
+	{
+		return \Profiler::log($type,$text,$time);
+	}
+
+	public static function s($dbname, $sql)
+	{
+		return \Profiler::start($dbname, $sql);
+	}
+
+	public static function sp($text=null)
+	{
+		return \Profiler::stop($text);
+	}
+
+}
